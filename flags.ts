58b146e85c268fbedb5d233faf4a55801e6a2219
Nandini Chatterjee <phy_nandini@icloud.com>
2025-05-07 13:14:46 +0100

    refactor: update weatherWidgetFlag to use environment variable for feature toggle

--- flags.ts
+++ flags.ts
@@ -1,11 +1,6 @@
-"use server";
-
 import { flag } from "flags/next";
 
-export const weatherWidgetFlag = flag({
+export const weatherWidgetFlag = flag<boolean>({
   key: "weather-widget",
-  decide() {
-    // Enable the weather widget for 50% of visitors
-    return Math.random() > 0.5;
-  },
+  decide: () => process.env.NEXT_PUBLIC_FEATURE_X_ENABLED === "true",
 });
